fe024a33c99c92420b3959178508ef0e75aaa937
andrew-1237 <akelly@beechhallschool.org>
2025-01-23 12:19:11 +0000

    changed input

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,9 @@ for rec in records:
     st.write(rec)
 
 
-pt = input("do you want to deit a user")
+pt = st.text_input("do you want to deit a user")
 if pt == "yes":
-    usersdelat = int(input("what user do you want to delete"))
+    usersdelat = st.int_input("what user do you want to delete")
     db.delete_record(usersdelat)
     st.write("Updated records:")
     records = db.read_records()
